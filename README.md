2c5adce35728baa4369145d308d886c71ffd04c8
Samuel Skånberg <samuel.skanberg@pingdom.com>
2015-01-19 16:56:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# hello world
+Demonstrates a Hello World app for Firefox OS that is installable
+
+Please see 
+
+* [https://developer.mozilla.org/en-US/Apps/Build/Manifest](https://developer.mozilla.org/en-US/Apps/Build/Manifest)
+* [https://marketplace.firefox.com/developers/validator](https://marketplace.firefox.com/developers/validator)
---
+++ app.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function() {
+    console.log("Hello World!");
+
+    // Install app
+    if (navigator.mozApps) {
+        var manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+        console.log("Manifest url: "+manifestURL);
+        var req = navigator.mozApps.installPackage(manifestURL);
+        req.onsuccess = function() {
+            console.log(this.result.origin);
+        };
+        req.onerror = function() {
+            console.log(this.error.name);
+        };
+    }
+});
